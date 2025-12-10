aad1f8e21104e0b5e9035f91c2c65c254fbf0e2b
igcogito <igorv3872@gmail.com>
2025-12-10 21:31:18 +0000

    Add OpenAPI annotations to ConversionController

--- src/main/java/com/igorcavalcanti/currency_converter_api/controller/ConversionController.java
+++ src/main/java/com/igorcavalcanti/currency_converter_api/controller/ConversionController.java
@@ -3,6 +3,9 @@ package com.igorcavalcanti.currency_converter_api.controller;
 
 import com.igorcavalcanti.currency_converter_api.dto.response.ConversionResponse;
 import com.igorcavalcanti.currency_converter_api.service.ConversionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,18 +18,27 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Currency Conversion", description = "Endpoints responsáveis pela conversão de moedas")
 public class ConversionController {
 
     private final ConversionService conversionService;
 
     @GetMapping("/convert")
+    @Operation(
+            summary = "Converte valores entre duas moedas",
+            description = "Realiza a conversao usando taaxas internas em memoria. Esse endpoint ja possui cache das taxas"
+    )
     public ResponseEntity<ConversionResponse> convert(
+            @Parameter(description = "Moeda de origem (ex: GBP)", example = "GBP")
             @RequestParam String from,
+
+            @Parameter(description = "Moeda de destino (ex: BRL)", example = "BRL")
             @RequestParam String to,
+
+            @Parameter(description = "Valor a ser convertido", example = "100.00")
             @RequestParam BigDecimal amount
 
     ) {
-        ConversionResponse response = conversionService.convert(from, to, amount);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(conversionService.convert(from, to, amount));
     }
 }
